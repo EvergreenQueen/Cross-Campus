4319fd48e089fe509a71126e6c5e70d7da0b5eaa
Marcel Relaford <iammarcel08@gmail.com>
2023-11-26 12:08:03 -0800

    added player class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using GlobalVars;
+
+public class Player
+{
+    private string name;
+    private Calendar playerCalendar;
+    private Course[] courses;
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public Course[] GetCourses() 
+    { 
+        return courses;
+    }
+}
