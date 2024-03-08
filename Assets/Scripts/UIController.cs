8a3b4d28343faacd4f20069914b6847f2b645b58
sidechara <creativenamegoeshere0@gmail.com>
2024-03-08 14:03:20 -0800

    Update UIController.cs
    added transition to different scene

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,10 +6,16 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour {
     public GameObject Square;
     bool change = false;
+    bool change2 = false;
+    int times = 0;
+
+    public Button start, quit;
 
     public void Update() {
         if (change) {
             StartCoroutine(FadeBlackSquare());
+        } else if (change2) {
+            StartCoroutine(FadeBlackSquare(false));
         }
     }
 
@@ -17,26 +23,40 @@ public class UIController : MonoBehaviour {
         change = true;
     }
 
-    public IEnumerator FadeBlackSquare(bool fade = true, int speed = 5) {
-        change = false;
+    public bool getChange() {
+        return change;
+    }
+
+    public IEnumerator FadeBlackSquare(bool fade = true, int speed = 3) {
         Color objectColor = Square.GetComponent<Image>().color;
         float fadeAmount;
-        Debug.Log("uh");
+        //int times = 0;
 
         if (fade) {
-            while (Square.GetComponent<Image>().color.a < 1) {
+            while (Square.GetComponent<Image>().color.a < 1 && change == true) {
                 fadeAmount = objectColor.a + (speed*Time.deltaTime);
+                times++;
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                Square.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+            change = false;
+            change2 = true;
+            start.gameObject.SetActive(false);
+            quit.gameObject.SetActive(false);
+        } else {
+            while (Square.GetComponent<Image>().color.a > 0 && change2 == true) {
+                //Debug.Break();
+                fadeAmount = objectColor.a - (speed*Time.deltaTime);
 
                 objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
                 Square.GetComponent<Image>().color = objectColor;
-                Debug.Log("got here");
                 yield return null;
             }
-
+            change2 = false;
         }
 
-
-        yield return new WaitForEndOfFrame();
+        //yield return new WaitForEndOfFrame();
     }
 
 
