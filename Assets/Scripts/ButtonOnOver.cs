c410a44ecf96d61f1baa7e0ddcb29a87bc08ba4f
clefalto <113867060+clefalto@users.noreply.github.com>
2024-03-14 22:59:54 -0700

    changed menu buttons to use better assets

--- Assets/Scripts/ButtonOnOver.cs
+++ Assets/Scripts/ButtonOnOver.cs
@@ -6,13 +6,23 @@ using TMPro;
 public class ButtonOnOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public TMP_Text text;
+    public GameObject selectedIcon;
+    public float iconOffsetX;
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        text.fontStyle |= FontStyles.Bold;
+        // text.fontStyle |= FontStyles.Bold;
+        text.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, 0.2f);
+        text.color = new Color32(0xEF, 0xEA, 0x99, 0xFF);
+        
+        selectedIcon.transform.position = new Vector3(text.transform.position.x + iconOffsetX, text.transform.position.y);
+        selectedIcon.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        text.fontStyle ^= FontStyles.Bold;
+        // text.fontStyle ^= FontStyles.Bold;
+        text.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, 0.0f);
+        text.color = new Color32(0xEF, 0x70, 0xC4, 0xFF);
+        selectedIcon.SetActive(false);
     }
 }
