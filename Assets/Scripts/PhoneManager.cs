3a5a8a3bde4ef7dc1aacfd319b51f7ddce3d4c5d
DerpyMissile <hmai015@ucr.edu>
2024-08-27 22:13:32 -0700

    phone mostly dun

--- Assets/Scripts/PhoneManager.cs
+++ Assets/Scripts/PhoneManager.cs
@@ -1,13 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PhoneManager : MonoBehaviour
 {
+    public GameObject scrollviewContent;
+    public GameObject mascotFinderTemplate;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mascot[] foundMascot = FindObjectsOfType<Mascot>();
+        for(int i=0; i<foundMascot.Length; ++i){
+            Debug.Log(foundMascot[i].mascotName);
+        }
+        for(int i=0; i<foundMascot.Length; ++i){
+            GameObject btn = (GameObject)Instantiate(mascotFinderTemplate);
+            btn.transform.SetParent(scrollviewContent.transform);
+            Transform txt = btn.transform.GetChild(0);
+            txt.GetComponent<TMP_Text>().text = foundMascot[i].mascotName;
+            int i2 = i;
+            btn.GetComponent<Button>().onClick.AddListener(delegate { findFren(foundMascot[i2].mascotName); });
+        }
+    }
+
+    void findFren(string s){
+        // mght reformat ths once we fgure out how the dates work
+        switch(s){
+            case "Scotty":
+                Debug.Log("Touched Scotty");
+                break;
+            case "Triton":
+                Debug.Log("Touched Triton");
+                break;
+            case "Sammy":
+                Debug.Log("Touched Sammy");
+                break;
+            case "Ru":
+                Debug.Log("Touched Ru");
+                break;
+            case "Peter":
+                Debug.Log("Touched Peter");
+                break;
+            case "Oski":
+                Debug.Log("Touched Oski");
+                break;
+            case "Oleander":
+                Debug.Log("Touched Oleander");
+                break;
+            case "Norm":
+                Debug.Log("Touched Norm");
+                break;
+            case "Josie":
+                Debug.Log("Touched Josie");
+                break;
+            case "Joe":
+                Debug.Log("Touched Joe");
+                break;
+            case "Gunrock":
+                Debug.Log("Touched Gunrock");
+                break;
+            case "":
+                break;
+        }
     }
 
     // Update is called once per frame
