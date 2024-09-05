ff506d44137c2b3cda4748f8536afcbd504b8790
Alvin <chenalvin337@gmail.com>
2024-09-04 18:17:02 -0700

    Added campus map menu ui and modified button hover to include replaceable sprites

--- Assets/Scripts/ButtonOnOver.cs
+++ Assets/Scripts/ButtonOnOver.cs
@@ -7,22 +7,41 @@ public class ButtonOnOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 {
     public TMP_Text text;
     public GameObject selectedIcon;
+    public Sprite hoveredSprite;
+    public Sprite unHoveredSprite;
     public float iconOffsetX;
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        // text.fontStyle |= FontStyles.Bold;
-        text.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, 0.2f);
-        text.color = new Color32(0xEF, 0xEA, 0x99, 0xFF);
-        
-        selectedIcon.transform.position = new Vector3(text.transform.position.x + iconOffsetX, text.transform.position.y);
-        selectedIcon.SetActive(true);
+        if (text)
+        {
+            // text.fontStyle |= FontStyles.Bold;
+            text.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, 0.2f);
+            text.color = new Color32(0xEF, 0xEA, 0x99, 0xFF);
+
+            selectedIcon.transform.position = new Vector3(text.transform.position.x + iconOffsetX, text.transform.position.y);
+            selectedIcon.SetActive(true);
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = hoveredSprite;
+            
+        }
     }
 
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        // text.fontStyle ^= FontStyles.Bold;
-        text.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, 0.0f);
-        text.color = new Color32(0xEF, 0x70, 0xC4, 0xFF);
-        selectedIcon.SetActive(false);
+        if (text)
+        {
+            // text.fontStyle ^= FontStyles.Bold;
+            text.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, 0.0f);
+            text.color = new Color32(0xEF, 0x70, 0xC4, 0xFF);
+            selectedIcon.SetActive(false);
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = unHoveredSprite;
+
+        }
+
     }
 }
