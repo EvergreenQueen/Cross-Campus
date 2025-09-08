ec6931f324d1dbea3915d6c3bdce8b67a59961ac
TechnicallyAWaffle <technicalwaffle@gmail.com>
2025-09-07 19:55:14 -0700

    Alvin - Audiomanager completed
    Audiomanager is a Don'tDestroyOnLoad singleton class with two public coroutines stoptracks and switchtracks. You can also directly call the public function playtrack. Playsfx is also a public function that plays an sfx.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Purchasing;
 
 public class AudioManager : MonoBehaviour
 {
-    public List<AudioClip> musicTracks = new List<AudioClip>();
-    public List<AudioClip> soundEffects = new List<AudioClip>();
+    //Music
+    [SerializeField] private List<AudioClip> timeOfDayMusicTracks = new List<AudioClip>();
+    private int currentTimeOfDayTrack;
+    public AudioClip titleScreenTrack;
+    public AudioClip orientationTrack;
+    public AudioClip dateTrack;
+    public AudioClip lifeEventTrack;
+    //SFX
+    public AudioClip clickSFX;
+    public AudioClip dialogueNextSFX;
+    public AudioClip hoverOverSFX;
+    [SerializeField] private float trackFadeOutTime;
+
 
     [SerializeField] private AudioSource musicSource;
     [SerializeField] private AudioSource sfxSource;
@@ -32,13 +44,17 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
-        PlayMusic(musicTracks[0]);
+        DontDestroyOnLoad(gameObject);
+        currentTimeOfDayTrack = 0;
+        //StartCoroutine(Test());
+        //PlayMusic(musicTracks[0]);
     }
 
-    public void PlayMusic(AudioClip musicTrack)
+    public void PlayTrack(AudioClip musicTrack)
     {
         musicSource.clip = musicTrack;
         musicSource.Play(0);
+        Debug.Log(musicSource.clip.name);
     }
 
     public void PlaySFX(AudioClip sfx)
@@ -47,13 +63,19 @@ public class AudioManager : MonoBehaviour
         sfxSource.Play(0);
     }
 
-    public IEnumerator StopMusic(float FadeTime)
+    public void SwitchTimeOfDayTrack()
+    {
+        currentTimeOfDayTrack += 1;
+        StartCoroutine(SwitchTracks(timeOfDayMusicTracks[currentTimeOfDayTrack]));
+    }
+
+    public IEnumerator StopTracks()
     {
         float startVolume = musicSource.volume;
 
         while (musicSource.volume > 0)
         {
-            musicSource.volume -= startVolume * Time.deltaTime / FadeTime;
+            musicSource.volume -= startVolume * Time.deltaTime / trackFadeOutTime;
 
             yield return null;
         }
@@ -61,5 +83,34 @@ public class AudioManager : MonoBehaviour
         musicSource.Stop();
         musicSource.volume = startVolume;
     }
+
+    public IEnumerator SwitchTracks(AudioClip newMusicTrack)
+    {
+        yield return StartCoroutine(StopMusic());
+        PlayTrack(newMusicTrack);
+    }
+
+    /*
+    private IEnumerator Test()
+    {
+        StartCoroutine(SwitchTracks(titleScreenTrack));
+        yield return new WaitForSeconds(3);
+        StartCoroutine(SwitchTracks(dateTrack));
+        yield return new WaitForSeconds(3);
+        StartCoroutine(SwitchTracks(orientationTrack));
+        yield return new WaitForSeconds(3);
+        StartCoroutine(StopMusic());
+        yield return new WaitForSeconds(3);
+        PlaySFX(dialogueNextSFX);
+        StartCoroutine(SwitchTracks(timeOfDayMusicTracks[0]));
+
+        for (int i = 0; i < timeOfDayMusicTracks.Count - 1; i++)
+        {
+            yield return new WaitForSeconds(3);
+            SwitchTimeOfDayTrack();
+        }
+        
+    }
+    */
 
 }
